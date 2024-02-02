eb3b86b647d2cfd2856f889cc55b0faa6fcdc01d
brentnettles <brentnettles@gmail.com>
2024-02-01 21:28:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// GLOBAL
+const leastFavoriteCustomer = 'someone';
+
+//Below Function does not work because the variable in the functions was determined perviously by const aka = Global
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
+
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
